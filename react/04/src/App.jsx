63471785ff9058bcf33ed296304cec8d5c9511ce
fkinga21 <kinga041121@gmail.com>
2026-04-16 14:20:08 +0200

    commit

--- react/04/src/App.jsx
+++ react/04/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Kalkulator from './components/Kalkulator';
 import SutiJatek from './components/SutiJatek';
 
